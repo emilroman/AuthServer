a0c623ca1fac07ce4a70fc231ff8de6a57edda14
emilroman <emir23us@yahoo.com>
2018-03-09 20:54:58 +0200

    webpack js files setup

---
+++ AuthServer/webpack.config.js
@@ -0,0 +1,13 @@
+﻿const path = require('path');
+
+const config = {
+    entry: {
+        login: './Content/Scripts/Login.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'wwwroot', 'scripts'),
+        filename: '[name].js'
+    }
+};
+
+module.exports = config;
