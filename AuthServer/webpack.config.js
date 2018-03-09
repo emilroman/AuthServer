187fdee9c89d63da247cd825ae874509b602a6b2
emilroman <emir23us@yahoo.com>
2018-03-09 23:48:38 +0200

    added webpack support for scss files

--- AuthServer/webpack.config.js
+++ AuthServer/webpack.config.js
@@ -1,20 +1,29 @@
 ﻿const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        login: './Content/Scripts/Login.js'
+        login: ['./Content/Scripts/Login.js', './Content/Styles/Login.scss']
     },
     output: {
         path: path.resolve(__dirname, 'wwwroot', 'scripts'),
-        filename: '[name].js'
+        filename: '[name].bundle.js'
     },
     module: {
         rules: [
             {
-                use: 'babel-loader',
                 test: /\.js$/,
-
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '../Styles/[name].bundle.css'
+        })
+    ]
 };
