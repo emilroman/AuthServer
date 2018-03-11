84be7fd855b54a16936599f22559aaac40cea0e2
emilroman <emir23us@yahoo.com>
2018-03-11 21:42:29 +0200

    Created RegisterAccountController wireframe

---
+++ AuthServer/Models/RegisterAccount/RegisterAccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.Models.RegisterAccount
+{
+    public class RegisterAccountViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(ConfirmPassword))]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
